deaa822e2e21366a17aa1515052d6f0d5ce2b719
Colby Allen <colbyallen012@gmail.com>
2019-07-28 10:10:14 -0600

    oneTeamData fetch test done

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -43,4 +43,80 @@ describe('apiCalls', () => {
       expect(getCrimesData()).rejects.toEqual(Error('Error fetching crimes'));
     })
   });
+
+  describe('getOneTeamData', () => {
+    let mockTeam;
+
+    beforeEach(() => {
+      mockTeam = [{
+        arrest_stats_id: "903",
+        Date: "2018-10-22",
+        Team: "DEN",
+        Team_name: "Broncos",
+        Team_preffered_name: "Denver Broncos",
+        Team_city: "Denver",
+        Team_logo_id: "10",
+        Team_Conference: "AFC",
+        Team_Division: "West",
+        Team_Conference_Division: "AFC West",
+        Team_hex_color: "FC4C02",
+        Team_hex_alt_color: "0C2340",
+        reddit_group_id: "3",
+        Name: "Chad Kelly",
+        Position: "QB",
+        Position_name: "Quarterback",
+        Position_type: "O",
+        Encounter: "Arrested",
+        Category: "Trespassing",
+        Crime_category: "Other",
+        Crime_category_color: "BCBD22",
+        Description: "The Englewood Colorado Police arrested and charged Kelly with first-degree criminal trespassing after a report of a man allegedly inside a home.",
+        Outcome: "Resolution Undetermined.",
+        Season: "2018",
+        ArrestSeasonState: "InSeason",
+        overall_category_id: "15",
+        general_category_id: "15",
+        legal_level_id: "1",
+        resolution_category_id: "1",
+        Year: "2018",
+        Month: "10",
+        Day: "22",
+        Day_of_Week: "Monday",
+        Day_of_Week_int: "2",
+        YearToDate: "0",
+        DaysSince: "244",
+        DaysToLastArrest: "36",
+        DaysToLastCrimeArrest: null,
+        DaysToLastTeamArrest: "229"
+      }]
+
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve(mockTeam)
+        });
+      });
+    });
+
+    it('should be called with the correct url', async () => {
+      const team = 'DEN'
+      const expected = `http://nflArrest.com/api/v1/team/arrests/${team}`;
+      getOneTeamData(team);
+      expect(window.fetch).toHaveBeenCalledWith(expected)
+    });
+
+    it('should return a parsed response', async () => {
+      const result = await getOneTeamData();
+      expect(result).toEqual(mockTeam)
+    })
+
+    it('should return an error response', async () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.reject({
+          message: 'Error fetching team'
+        })
+      });
+
+      expect(getOneTeamData()).rejects.toEqual(Error('Error fetching team'));
+    })
+  })
 })
