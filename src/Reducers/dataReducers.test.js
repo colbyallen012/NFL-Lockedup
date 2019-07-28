574e2a2e7e60b0cd12e5b4afedb1de1f730056bb
Colby Allen <colbyallen012@gmail.com>
2019-07-28 12:25:57 -0600

    pass second crimeReducer test

--- src/Reducers/dataReducers.test.js
+++ src/Reducers/dataReducers.test.js
@@ -7,4 +7,15 @@ describe('crimeReducer', () => {
 
     expect(result).toEqual(expected)
   })
+
+  it('should return the state with crimes array', () => {
+    const expected = [{crime: 'DUI', arrests: 20}]
+    const action = {
+      type: 'SHOW_CRIMES',
+      crimes: [{crime: 'DUI', arrests: 20}]
+    }
+    const result = crimeReducer([], action)
+
+    expect(result).toEqual(expected)
+  })
 })
