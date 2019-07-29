d1a0517d29ffcef1a55e40114af538e07d82b3e1
Colby Allen <colbyallen012@gmail.com>
2019-07-29 10:29:11 -0600

    css to team container and team arrest cards

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -5,9 +5,9 @@ const Team = ({player, position, arrest, description, outcome, season}) => {
 
   return (
     <div className='team'>
-      <h3>{player}</h3>
-      <p><span className='bolder'>{position}</span></p>
-      <p><span className='bolder'>Charge: </span>{arrest} <span className='bolder'>Outcome: </span>{outcome}</p>
+      <h3 className='player'>{player} <span className='lighter'>{position}</span></h3>
+      <p><span className='bolder'>Charge: </span>{arrest}</p>
+      <p><span className='bolder'>Outcome: </span>{outcome}</p>
       <p><span className='bolder'>Description: </span>{description}</p>
       <p><span className='bolder'>Season: </span>{season}</p>
     </div>
